1a3062342f54a52f67444bcf5f9ce17b677fd3bd
Suresh Burra <surigoud2004@gmail.com>
2026-05-04 07:26:36 +0000

    const firebaseConfig = {   apiKey: "AIzaSyAykJ2K3WLRMGp8Lxd02JqEgTZFpxV-

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   apiKey: "AIzaSyAykJ2K3WLRMGp8Lxd02JqEgTZFpxV-O3A",
-  authDomain: "stellar-shift.firebaseapp.com",
-  projectId: "stellar-shift",
-  storageBucket: "stellar-shift.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  authDomain: "studio-547476889-b1c50.firebaseapp.com",
+  databaseURL: "https://studio-547476889-b1c50-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "studio-547476889-b1c50",
+  storageBucket: "studio-547476889-b1c50.firebasestorage.app",
+  messagingSenderId: "943623815632",
+  appId: "1:943623815632:web:d85d7811adac7e629a79d5"
 };
